4a40981320fcf644e945ccfa17f3fe87040821b0
Thomas <44287699+Poussinators@users.noreply.github.com>
2019-11-22 09:34:37 +0100

    Ajout page creation

---
+++ Mar.io_K0rT_WpfApp/Mar.io_K0rT_WpfApp/CreationWindows.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mar.io_K0rT_WpfApp
+{
+    /// <summary>
+    /// Logique d'interaction pour CreationWindows.xaml
+    /// </summary>
+    public partial class CreationWindows : Window
+    {
+        public CreationWindows()
+        {
+            InitializeComponent();
+        }
+    }
+}
